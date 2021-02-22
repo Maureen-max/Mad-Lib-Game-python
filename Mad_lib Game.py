7910f1d9ea228bfaadfe1138427a0cc3499e6220
Maureen-max <77680352+Maureen-max@users.noreply.github.com>
2021-02-22 03:44:24 +0300

    Add files via upload

---
+++ Mad_lib Game.py
@@ -0,0 +1,7 @@
+color = input("Enter a color :")
+curtains = input("Enter the curtain type :")
+name = input("what is your name :")
+
+print("Roses are color", color)
+print("The curtain type is",curtains)
+print("My name is", name)
